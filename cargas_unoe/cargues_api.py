8d32ebd367e6536a5f8bda1af8752de111c6e41b
Oscar Orozco <oscarorozco@MacBook-Air.local>
2025-06-24 21:31:44 -0500

    Continuar Cambios

--- cargas_unoe/cargues_api.py
+++ cargas_unoe/cargues_api.py
@@ -79,36 +79,48 @@ def enviar_datos_a_siesa():
         # 2. Procesar cada registro
         with engine.begin() as conn:  # begin => commit automático
             for _, row in df.iterrows():
-                
-                filtro = (
-                (df_op["docto"] == row["Docto"]) &
-                (df_op["tipo_inv"] == row["tipo_inv"]) &
-                (df_op["und_medida"] == row["und"])
-                )
 
-                item=df_op.loc[filtro,"item"].values[0]
-                lote=df_op.loc[filtro,"lote"].values[0]
+                try:
+
+                    filtro = (
+                    (df_op["docto"] == row["Docto"]) &
+                    (df_op["tipo_inv"] == row["tipo_inv"]) &
+                    (df_op["und_medida"] == row["und"])
+                    )
+
+                    if df_op.loc[filtro].empty:
+                        print(f"No se encontraron resultados para Docto {row['Docto']}")
+                        continue  # Salta a la siguiente fila
+
+                    item=df_op.loc[filtro,"item"].values[0]
+                    lote=df_op.loc[filtro,"lote"].values[0]
+                    ext1=df_op.loc[filtro,"ext1"].values[0]
+                    ext2=df_op.loc[filtro,"ext2"].values[0]
+                    bodega=df_op.loc[filtro,"bodega"].values[0]
+                except Exception as e:
+                    print(f"No se encontraron resultados {row['Docto']} : {e}")
+                    continue
 
                 payload = {
-                        #Documento
-                            "f350_id_tipo_docto": "EPP",
-                            "f350_consec_docto": 1,
-                            "f350_fecha": row["Fecha"].strftime('%Y%m%d') if isinstance(row["Fecha"], datetime) else row["Fecha"],
-                            "f350_notas": row["operario"]+row["Maquina"],
-                        #Movimiento
-                            "f470_id_tipo_docto": "OPK",
-                            "f470_consec_docto": 1,
-                            "f470_nro_registro": 1,
-                            "f850_consec_docto": row["Docto"],
-                            "f470_id_item": item,
-                            "f470_referencia_item": "",
-                            "f470_codigo_barras": "",
-                            "f470_id_ext1_detalle": row["ext1"],
-                            "f470_id_ext2_detalle": row["ext2"],
-                            "f470_id_bodega": row["Bodega"],
-                            "f470_id_lote": lote,
-                            "f470_cant_base_entrega": row["Cantidad"]
-                            }
+                    #Documento
+                        "f350_id_tipo_docto": "EPP",
+                        "f350_consec_docto": 1,
+                        "f350_fecha": row["Fecha"].strftime('%Y%m%d') if isinstance(row["Fecha"], datetime) else row["Fecha"],
+                        "f350_notas": row["operario"]+" "+row["Maquina"],
+                    #Movimiento
+                        "f470_id_tipo_docto": "OPK",
+                        "f470_consec_docto": 1,
+                        "f470_nro_registro": 1,
+                        "f850_consec_docto": row["Docto"],
+                        "f470_id_item": item,
+                        "f470_referencia_item": "",
+                        "f470_codigo_barras": "",
+                        "f470_id_ext1_detalle": ext1,
+                        "f470_id_ext2_detalle": ext2,
+                        "f470_id_bodega": bodega,
+                        "f470_id_lote": lote,
+                        "f470_cant_base_entrega": row["Cantidad"]
+                        }
                 
                 print(payload)
 
