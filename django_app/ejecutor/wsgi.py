bb4cb5043594a249ec285c98c9dfcc08601c0b4a
Oscar Orozco <Oscaro@koscolombia.local>
2025-07-25 15:01:37 -0500

    Cambio nombres carpetas

--- django_app/ejecutor/wsgi.py
+++ django_app/ejecutor/wsgi.py
@@ -7,7 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 import os
+import sys
 
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ejecutor.settings')
