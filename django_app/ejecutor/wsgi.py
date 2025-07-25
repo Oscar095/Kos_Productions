f67ebdc216bc15f58f5d822dc84ebd27771b944b
Oscar Orozco <Oscaro@koscolombia.local>
2025-07-25 11:44:26 -0500

    Cambio nombres carpetas

--- django_app/ejecutor/wsgi.py
+++ django_app/ejecutor/wsgi.py
@@ -6,8 +6,10 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
-
+import sys
 import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), 'django_app'))
 
 from django.core.wsgi import get_wsgi_application
 
