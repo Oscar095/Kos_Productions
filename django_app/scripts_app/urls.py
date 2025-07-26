3608c21461f461bd498c1a5c89970c5273d9f89b
Oscar Orozco <oscarorozco@MacBook-Air.local>
2025-07-26 16:48:18 -0500

    Incluir Logo

--- django_app/scripts_app/urls.py
+++ django_app/scripts_app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('run/<int:script_id>/', views.run_script, name='run_script'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
 ]
